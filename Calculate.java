b8dfdf1dbc880fd1cceaa774299650e12b830332
jaytalla <jay.ray.talla06102000@gmail.com>
2024-06-25 12:40:51 +0800

    Added java version

---
+++ Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+
+    public static void main(String[] args) {
+
+        String sizeSmall = ["5", "5000", "S"];
+        String sizeMedium = ["10", "8000", "S"];
+        String sizeLarge = ["15", "12000", "S"];
+
+        System.out.print("Hello");
+    }
+
+}
